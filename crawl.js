da5b08690dc4db402ff2f4ad74a9861314a4a150
iulian <iulian.on@vebo.io>
2024-02-21 11:06:34 +0200

    create get urls from html functionality with tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 function normalizeURL(url) {
   const urlObj = new URL(url)
   const hostname = urlObj.hostname
@@ -7,7 +9,17 @@ function normalizeURL(url) {
   return `${hostname}${pathname}`
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+  const htmlObj = new JSDOM(htmlBody)
+  const urls = htmlObj.window.document.querySelectorAll('a')
+  const absoluteUrls = [...urls].map((item) => {
+    const href = item.href
+    return new URL(href, baseURL).href
+  })
+  return absoluteUrls
+}
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
+// test normalize url function
 
 test('normalize url https://blog.boot.dev/path/ to blog.boot.dev/path', () => {
   expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
@@ -16,4 +17,27 @@ test('normalize url http://blog.boot.dev/path/ to blog.boot.dev/path', () => {
 
 test('normalize url http://blog.boot.dev/path to blog.boot.dev/path', () => {
   expect(normalizeURL('http://blog.boot.dev/path')).toBe('blog.boot.dev/path')
+})
+
+// test get absolute urls from html function
+
+test('get urls from html body', () => {
+  const htmlBody = `
+    <html>
+      <body>
+        <a href="/home"><span>Go to Boot.dev</span></a>
+        <a href="http://blog.boot.dev/index.html"><span>Go to Boot.dev</span></a>
+        <a><span>Go to Boot.dev</span></a>
+        <a href="https://blog.boot.dev/test/absolute"><span>Go to Boot.dev</span></a>
+      </body>
+    </html>`
+
+  const urls = [
+    'https://blog.boot.dev/home',
+    'http://blog.boot.dev/index.html',
+    'https://blog.boot.dev/',
+    'https://blog.boot.dev/test/absolute'
+  ]
+
+  expect(getURLsFromHTML(htmlBody, 'https://blog.boot.dev')).toStrictEqual(urls)
 })
